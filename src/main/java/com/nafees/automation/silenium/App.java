999a5c2a321dba7717b81bce666991705c94c8a5
nafeeskhan86 <nafees.mca07@gmail.com>
2021-11-03 20:59:45 +0530

    first commit

---
+++ src/main/java/com/nafees/automation/silenium/App.java
@@ -0,0 +1,13 @@
+package com.nafees.automation.silenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nafees/automation/silenium/TestSelenium.java
@@ -0,0 +1,15 @@
+package com.nafees.automation.silenium;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test; 
+
+public class TestSelenium {
+
+	WebDriver driver;
+	
+	
+	@Test
+	public void setup() {
+		System.out.println("test");
+	}
+}
